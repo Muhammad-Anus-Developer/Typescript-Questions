ee954d7e29a331a3f04de42e75931dac0a181965
Muhammad Anus <anasafzal779@gmail.com>
2024-02-27 00:45:07 +0500

    Update question30.ts

---
+++ question30.ts
@@ -0,0 +1,8 @@
+let userName: string[] = [`admin`, `anasafzal779`, `anu779`, `anus779`,`adminboy`];
+for(let i = 0; i < userName.length; i++){
+    if(userName[i] === 'admin'){
+        console.log(`Hello admin, would you like to see a status report?`);
+    } else {
+        console.log(`Hello ${userName[i]}, thank you for logging in again.`);
+    }
+}
